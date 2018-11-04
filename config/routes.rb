bcd373107a2cfab2f8781147e09d46ce6128ff80
Marcus Hamill <marcusehamill@gmail.com>
2018-11-04 12:26:47 -0600

    add new and create html actions for products

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # STEP 1: A ROUTE triggers a controller action
   # verb "/urls" => "namespace/controllers#action"
-  get 'products' => 'products#index'
+  get '/products' => 'products#index'
+  get '/products/new' => 'products#new'
+  get '/products/:id' => 'products#show'
+  post '/products' => 'products#create'
   namespace :api do
     get '/products/:id' => 'products#show'
     get '/products' => 'products#index'
