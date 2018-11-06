00484285d1c86374d4e80833ab3d0b34e8471af3
Marcus Hamill <marcusehamill@gmail.com>
2018-11-05 19:30:08 -0600

    add edit, update, and destroy routes and methods to product controller for html view.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -23,4 +23,25 @@ class ProductsController < ApplicationController
     @product.save!
     redirect_to "/products/#{@product.id}"
   end
+
+  def edit
+    @product = Product.find_by(id: params[:id])
+    render 'edit.html.erb'
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.name = params[:name]
+    @product.price = params[:price]
+    @product.description = params[:description]
+    @product.supplier_id = params[:supplier_id]
+    @product.save
+    redirect_to "/products/#{@product.id}"
+  end
+
+  def destroy
+    @product = Product.find_by(id: params[:id])
+    @product.destroy
+    redirect_to "/products"
+  end
 end
